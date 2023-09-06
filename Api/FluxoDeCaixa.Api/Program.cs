fb48275ec3517c1369a9462b1643f2716fb9246b
Cristiano Cunha <contato@cristianoprogramador.com>
2023-09-06 08:37:47 -0300

    Cria base da API

---
+++ Api/FluxoDeCaixa.Api/Program.cs
@@ -0,0 +1,44 @@
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((builderContext, _, configuration) =>
+{
+    if (builderContext.HostingEnvironment.IsDevelopment())
+        configuration.WriteTo.Console(theme: AnsiConsoleTheme.Literate);
+    else
+        configuration.WriteTo.File("./logs.txt");
+    //em um ambiente real o ideal seria gravar em um serviço externo
+});
+
+const string corsConfig = "_personalDomainCors";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: corsConfig,
+        policy =>
+        {
+            //Em um ambiente real precisaríamos configurar uma para dev e uma para prod com as origens corretas
+            policy
+                .WithOrigins("*")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
