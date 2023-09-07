149d4ffc5c28039b379c5dc2225705147cd13e15
Cristiano Cunha <contato@cristianoprogramador.com>
2023-09-07 10:33:47 -0300

    Cria balance controller

---
+++ Api/FluxoDeCaixa.Api/Controllers/BalanceController.cs
@@ -0,0 +1,41 @@
+using FluxoDeCaixa.Application.Services.Balance;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluxoDeCaixa.Api.Controllers;
+
+[Route("/api/[controller]")]
+public class BalanceController : Controller
+{
+    private readonly IBalanceApplicationService _service;
+    private readonly ILogger<BalanceController> _logger;
+
+    public BalanceController(IBalanceApplicationService service, ILogger<BalanceController> logger)
+    {
+        _service = service;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateBalanceCommand command)
+    {
+        try
+        {
+            var balance = await _service.Create(command);
+            if (balance.IsValid)
+                return BadRequest(balance);
+            return Created(string.Empty, balance);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Erro ao criar um novo balance");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var balances = await _service.Get();
+        return Ok(balances);
+    }
+}
