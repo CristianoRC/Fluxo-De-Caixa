9f0c45f82dfe40d7f92bd8e1c8014e9a930f9697
Cristiano Cunha <contato@cristianoprogramador.com>
2023-09-07 14:25:07 -0300

    Cria config base de transação

---
+++ Api/FluxoDeCaixa.Infra/Configuration/TransactionConfiguration.cs
@@ -0,0 +1,18 @@
+using FluxoDeCaixa.Domain.Entities;
+using FluxoDeCaixa.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FluxoDeCaixa.Infra.Configuration;
+
+public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Type);
+        builder.Property(x => x.CreatedAt);
+        builder.Property(x => x.BalanceAfterTransaction).HasConversion(x => x.Value, amount => new BalanceAmount(amount));
+        builder.HasOne(x => x.Balance);
+    }
+}
