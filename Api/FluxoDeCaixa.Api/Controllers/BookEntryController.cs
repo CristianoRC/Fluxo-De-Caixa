09f6771403c0c62ea47b595249b83dd1303f641b
Cristiano Cunha <contato@cristianoprogramador.com>
2023-09-07 10:49:41 -0300

    Implementa BookEntryController

---
+++ Api/FluxoDeCaixa.Api/Controllers/BookEntryController.cs
@@ -0,0 +1,39 @@
+using FluxoDeCaixa.Application.Services.BookEntry;
+using FluxoDeCaixa.Domain.Services.BookEntry;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluxoDeCaixa.Api.Controllers;
+
+[Route("/api/[controller]")]
+public class BookEntryController : Controller
+{
+    private readonly IBookEntryApplicationService _service;
+    private readonly ILogger<BookEntryController> _logger;
+
+    public BookEntryController(IBookEntryApplicationService service, ILogger<BookEntryController> logger)
+    {
+        _service = service;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateBookEntry command)
+    {
+        try
+        {
+            var bookEntry = await _service.Create(command);
+            if (bookEntry.Errors.Any())
+                return BadRequest(bookEntry);
+            return Created(string.Empty, bookEntry);
+        }
+        catch (ArgumentException e)
+        {
+            return BadRequest(e.Message);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Erro ao criar um novo book entry");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
