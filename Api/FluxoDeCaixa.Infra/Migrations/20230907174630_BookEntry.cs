8479c93e7e1a9608c78a9b5448b01af4b6240a05
Cristiano Cunha <contato@cristianoprogramador.com>
2023-09-07 14:48:25 -0300

    cria migração do book entry

---
+++ Api/FluxoDeCaixa.Infra/Migrations/20230907174630_BookEntry.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FluxoDeCaixa.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookEntries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EntryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OffsetId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookEntries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookEntries_Transaction_EntryId",
+                        column: x => x.EntryId,
+                        principalTable: "Transaction",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookEntries_Transaction_OffsetId",
+                        column: x => x.OffsetId,
+                        principalTable: "Transaction",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookEntries_EntryId",
+                table: "BookEntries",
+                column: "EntryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookEntries_OffsetId",
+                table: "BookEntries",
+                column: "OffsetId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookEntries");
+        }
+    }
+}
