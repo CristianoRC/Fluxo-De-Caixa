89e29738ac552b5571cf5662a4bd2af612f587bd
Cristiano Cunha <contato@cristianoprogramador.com>
2023-09-09 09:47:38 -0300

    Adiciona configuração do MongoDB

--- Api.Report/FluxoDeCaixa.Api.Report.Infra/Setup.cs
+++ Api.Report/FluxoDeCaixa.Api.Report.Infra/Setup.cs
@@ -1,5 +1,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 namespace FluxoDeCaixa.Api.Report.Infra;
 
@@ -7,6 +11,17 @@ public static class Setup
 {
     public static IServiceCollection AddInfra(this IServiceCollection service, IConfiguration configuration)
     {
+        ConfigureMongoDb(service, configuration);
         return service;
+    }
+    
+    private static void ConfigureMongoDb(IServiceCollection services, IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration["MongoDb"]);
+        services.AddSingleton<IMongoClient>(client);
+        
+        var objectDiscriminatorConvention = BsonSerializer.LookupDiscriminatorConvention(typeof(object));
+        var objectSerializer = new ObjectSerializer(objectDiscriminatorConvention, GuidRepresentation.Standard);
+        BsonSerializer.RegisterSerializer(objectSerializer);
     }
 }
